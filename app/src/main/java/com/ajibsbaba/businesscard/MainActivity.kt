a10fda0b5484706042c8b0e0940b1aeebdab8734
Samuel Ajibade <samuelajibade22@gmail.com>
2022-03-16 11:01:59 +0100

    created composables added content styled composables using modifiers

--- app/src/main/java/com/ajibsbaba/businesscard/MainActivity.kt
+++ app/src/main/java/com/ajibsbaba/businesscard/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.ajibsbaba.businesscard
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ajibsbaba.businesscard.ui.theme.Blue100
 import com.ajibsbaba.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +33,76 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BusinessCardTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard()
                 }
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BusinessCard() {
+    Column(
+        verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.Start ) {
+        Column(horizontalAlignment = Alignment.Start) {
+            Text(text = "Samuel Ajibade", color = Color.Black, fontWeight = FontWeight.ExtraBold, fontSize = 30.sp)
+            Text(text = "Mobile Engineer", color = MaterialTheme.colors.onSurface)
+            Text(text = "A developer concerned with making lives easier")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
+fun ProfileImage() {
+    Box(modifier = Modifier.padding(top = 12.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ajibsbaba),
+            contentDescription = "Profile Image", modifier = Modifier.size(130.dp)
+        )
+    }
+}
+
+
+@Preview()
+@Composable
+fun PortfolioButton(ButtonText: String? = "Contact Me") {
+    Button(modifier = Modifier
+        .width(200.dp)
+        .height(45.dp), colors = ButtonDefaults.buttonColors(
+        backgroundColor = Blue100, contentColor = Color.White
+        ),  shape = RoundedCornerShape(10.dp), onClick = { Log.d(TAG, "Create Projects Card")}) {
+        if (ButtonText != null) {
+            Text(text = ButtonText, fontWeight = FontWeight.SemiBold)
+        }
+    }
+}
+
+
+
+@Preview()
+@Composable
+fun ProjectCard() {
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
 fun DefaultPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+        Column(verticalArrangement = Arrangement.spacedBy(12.dp), modifier = Modifier
+            .padding(start = 20.dp, end = 20.dp)
+            .fillMaxWidth()) {
+            ProfileImage()
+            BusinessCard()
+            PortfolioButton()
+        }
     }
 }
